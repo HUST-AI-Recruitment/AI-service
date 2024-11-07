b4b380aeeac83a7287c878349e0cea7d75730eb5
Liubainian <1029922060@qq.com>
2024-11-07 19:39:28 +0800

    Router Modified for new API

--- service/job.py
+++ service/job.py
@@ -1,24 +1,33 @@
-def describe_job(position, mode = 'job-recommend' or 'candidate-recommend'):
+allowing_modes = ['recommend-resume', 'recommend-description', 'rank-candidates']
+
+def describe_job(position, mode):
+    assert mode in allowing_modes
     id = position['id']
     title = position['title']
     description = position['description']
+    demand = position['demand']
     location = position['location']
     salary = position['salary']
-    company = position['company_name']
-    created = position['create_at']
-    if mode == 'job-recommend':
+    company = position['company']
+    # created = position['create_at']
+    job_type = position['job_type']
+    owner_id = position['owner_id']
+    if mode in ['recommend-resume', 'recommend-description']:
         return f"""
-        This is one of the jobs to be considered by one person, please consider the job description and the resume file,
+        In the following is one of the jobs to be considered, its id is {id}, please consider the job description and my resume,
         rate this job by 1-100, 100 is the best match, 1 is the worst match. Just answer one number as score.
-        The job title is {title}, the company requires {description}, and the work location is {location}.
-        The salary is {salary} RMB per month, the company name is {company}.
+        The job title is {title}, and can be described as {description}, and the work location is {location}.
+        The company requires {demand}. While the salary is {salary} RMB per month, the company name is {company}.
         """
-    elif mode == 'candidate-recommend':
+    elif mode == 'rank-candidates':
         return f"""
-        This is one of the candidates to be considered by the company, please consider the job description and the resume file,
-        rate this job by 1-100, 100 is the best match, 1 is the worst match. Just answer one number as score.
-        The job title is {title}, the company requires {description}, and the work location is {location}.
-        The salary is {salary} RMB per month, the company name is {company}.
+        This is one of the candidates to be considered by the company, please consider the job description and the resume,
+        rate the candidate by 1-100, 100 is the best match, 1 is the worst match. Just answer one number as score.
+        The job title is {title}, and can be described as {description}, and the work location is {location}.
+        The company requires {demand}. While the salary is {salary} RMB per month, the company name is {company}.
         """
-    else:
-        raise ValueError("mode should be 'job-recommend' or 'candidate-recommend'")
+
+
+def describe_player(resume, mode):
+    assert mode in allowing_modes
+    raise NotImplementedError
